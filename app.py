71117279f20868f0847b60d4bb50759aa282314e
Yash Mane <yashmane9872@gmail.com>
2026-05-02 11:14:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ REGION = "us-east-1"
 
 def get_conn():
     # Generate the IAM Token dynamically for every connection
-    client = boto3.client('rds', region_name=REGION)
+    client = boto3.client('rds', region_name=REGION),
     token = client.generate_db_auth_token(DBHostname=DB_HOST, Port=3306, DBUsername=DB_USER)
     
     return pymysql.connect(
