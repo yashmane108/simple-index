84751b1a2b21b9f26746c785f08a3e23840acd1b
Yash Mane <yashmane9872@gmail.com>
2026-05-03 10:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_conn():
     return pymysql.connect(
         host=DB_HOST,
         user=DB_USER,      # Ensure this is "admin"
-        password="NewAdminPass123!", # Your new master password
+        password="Success2026!", # Your new master password
         database=DB_NAME,
         port=3306,
         charset='utf8mb4',
